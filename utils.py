4f6d7f52fbf50b289bc31396b25bbea65d1546dc
Paramesh Chandra <51922459+paramphy@users.noreply.github.com>
2020-07-14 02:37:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -30,7 +30,7 @@ def normal_extrude(excount,o):
         bpy.ops.transform.resize(value=(.9, .9, .9), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
         print(count)
         count += 1
-    print("Done")
+    print("Done Normal Extruding")
 
 def object_extrude():
     
@@ -42,25 +42,23 @@ def object_extrude():
 
     for face in bm.faces:
         i += 1
-        print(i)
-            
+        #print(i)
+         
     rand_face = random.randint(0,i)
-
     print(rand_face)
     for face in bm.faces:
         face.select = False
-        print(face.index)
+        #print(face.index)
 
     bm.faces.ensure_lookup_table()
     bm.faces[rand_face].select = True
     count = 0
-    #random_extrude(co = 20)
     normal_extrude(20,count)
     bmesh.update_edit_mesh(me, True)
-    #bpy.ops.object.editmode_toggle()
-    count = 0
     
-def lightning(location=(0, 0, 0)):
+    
+    
+def lightning_mesh(location=(0, 0, 0)):
     
     bpy.ops.mesh.primitive_cube_add(size=5, enter_editmode=True, align='WORLD', location=(0, 0, 0))
     count = 0
@@ -72,10 +70,53 @@ def lightning(location=(0, 0, 0)):
     bpy.ops.object.editmode_toggle()
     bpy.ops.object.modifier_add(type='SUBSURF')
     bpy.ops.object.shade_smooth()
+    bpy.ops.object.modifier_add(type='BUILD')
+    bpy.context.object.modifiers["Build"].frame_duration = 20
     print("Lightning is summoned")
 
-count = 0 
-while count < 10:
-    lightning()
-    count += 1
+#return("Lightning is summoned")
+    
+#Material
+def lightning_material():
+    so = bpy.context.active_object
+    l_mat = bpy.data.materials.new(name = "Lightning Material")
+    so.data.materials.append(l_mat)
+    l_mat.use_nodes = True
+    nodes =l_mat.node_tree.nodes
+    material_output = nodes.get("Material Output")
+    node_emission = nodes.new(type = 'ShaderNodeEmission')
+
+    node_emission.inputs[0].default_value = [0,0.3,1,1]
+    node_emission.inputs[1].default_value = 1
+    
+    links = l_mat.node_tree.links
+    new_link = links.new(node_emission.outputs[0],material_output.inputs[0])
+    bpy.context.scene.eevee.use_bloom = True
+    
+    node_emission.inputs[1].keyframe_insert(data_path="default_value", frame=1)
+    node_emission.select = True
+    #print(node_emission.name)
+    dpath = 'nodes["Emission"].inputs[1].default_value'
+
+    fc = l_mat.node_tree.animation_data.action.fcurves.find(dpath)
+    if fc:
+        nmod = fc.modifiers.new("NOISE")
+        nmod.scale = 10
+        nmod.phase = 0
+        nmod.strength = 200
+        nmod.depth = 1.35
+        nmod.offset = 0.23
+    
+
+    
+def lightning():
+
+    lightning_mesh()
+    lightning_material()
+    
+lightning()
+
+
+
+
     
