361d04110482b82e694d6ae8eabdd831a0b39b40
Paramesh Chandra <parameshchandra28@gmail.com>
2021-05-02 18:08:44 +0530

    Update addon_add_lightning.py

--- addon_add_lightning.py
+++ addon_add_lightning.py
@@ -11,7 +11,7 @@ bl_info = {
 }
 
 
-import bpy,bmesh
+import bpy, bmesh
 from bpy.types import Operator
 from bpy.props import FloatVectorProperty
 from bpy_extras.object_utils import AddObjectHelper, object_data_add
@@ -19,104 +19,151 @@ from mathutils import Vector
 import random
 
 
+# Normal Extrude Function
 
-        
-        
-#Normal Extrude Function
 
-def normal_extrude(excount,o):
-    
-    #Initializing Count
-    
+def normal_extrude(excount, o):
+
+    # Initializing Count
+
     count = 0
-    
-    #Extuding Normal to Plane and Scaling
-    
+
+    # Extuding Normal to Plane and Scaling
+
     while count < excount:
-        bpy.ops.mesh.extrude_region_shrink_fatten(MESH_OT_extrude_region={"use_normal_flip":False, "mirror":False}, TRANSFORM_OT_shrink_fatten={"value":1, "use_even_offset":False, "mirror":False, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "release_confirm":True, "use_accurate":False})
-        bpy.ops.transform.translate(value=(2*random.random(), 2*random.random(), o), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, release_confirm=True)
-        bpy.ops.transform.resize(value=(.9, .9, .9), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
+        bpy.ops.mesh.extrude_region_shrink_fatten(
+            MESH_OT_extrude_region={"use_normal_flip": False, "mirror": False},
+            TRANSFORM_OT_shrink_fatten={
+                "value": 1,
+                "use_even_offset": False,
+                "mirror": False,
+                "use_proportional_edit": False,
+                "proportional_edit_falloff": "SMOOTH",
+                "proportional_size": 1,
+                "use_proportional_connected": False,
+                "use_proportional_projected": False,
+                "snap": False,
+                "snap_target": "CLOSEST",
+                "snap_point": (0, 0, 0),
+                "snap_align": False,
+                "snap_normal": (0, 0, 0),
+                "release_confirm": True,
+                "use_accurate": False,
+            },
+        )
+        bpy.ops.transform.translate(
+            value=(2 * random.random(), 2 * random.random(), o),
+            orient_type="GLOBAL",
+            orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)),
+            orient_matrix_type="GLOBAL",
+            constraint_axis=(False, True, False),
+            mirror=True,
+            use_proportional_edit=False,
+            proportional_edit_falloff="SMOOTH",
+            proportional_size=1,
+            use_proportional_connected=False,
+            use_proportional_projected=False,
+            release_confirm=True,
+        )
+        bpy.ops.transform.resize(
+            value=(0.9, 0.9, 0.9),
+            orient_type="GLOBAL",
+            orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)),
+            orient_matrix_type="GLOBAL",
+            mirror=True,
+            use_proportional_edit=False,
+            proportional_edit_falloff="SMOOTH",
+            proportional_size=1,
+            use_proportional_connected=False,
+            use_proportional_projected=False,
+        )
         count += 1
     print("Done Normal Extruding")
 
-#Getting the Planes and Extrude
+
+# Getting the Planes and Extrude
+
 
 def object_extrude():
-    
-    #Getting the active object
-    
+
+    # Getting the active object
+
     obj = bpy.context.active_object
     me = obj.data
     print("Object Detected")
     bm = bmesh.from_edit_mesh(me)
-    
-    #Initializing i
-    
+
+    # Initializing i
+
     i = 0
-    
-    #Counting the total number of faces 
-    
+
+    # Counting the total number of faces
+
     for face in bm.faces:
         i += 1
-        
-    #Choosing a random face to extrude 
-            
-    rand_face = random.randint(0,i-1)
-        
-    #Deselecting All faces
-    
+
+    # Choosing a random face to extrude
+
+    rand_face = random.randint(0, i - 1)
+
+    # Deselecting All faces
+
     for face in bm.faces:
         face.select = False
-        
-    #Selcting the random face 
-       
+
+    # Selcting the random face
+
     bm.faces.ensure_lookup_table()
     bm.faces[rand_face].select = True
     count = 0
-    
-    #Normal Extruding
-    
-    normal_extrude(20,count)
+
+    # Normal Extruding
+
+    normal_extrude(20, count)
     bmesh.update_edit_mesh(me, True)
-    
-    
-#Creating Lightning Mesh  
-   
+
+
+# Creating Lightning Mesh
+
+
 def lightning_mesh(location=(0, 0, 0)):
-    
-    #adding a cube
-    
-    bpy.ops.mesh.primitive_cube_add(size=5, enter_editmode=True, align='WORLD', location=(0, 0, 0))
+
+    # adding a cube
+
+    bpy.ops.mesh.primitive_cube_add(
+        size=5, enter_editmode=True, align="WORLD", location=(0, 0, 0)
+    )
     count = 0
-    
-    #Extruding Cube Object
-    
+
+    # Extruding Cube Object
+
     while count < 15:
         object_extrude()
         count += 1
-        
-        
+
     bpy.ops.object.editmode_toggle()
-    
-    #Adding Modifiers
-    
-    #Displacement Modifier
-    
-    bpy.ops.object.modifier_add(type='DISPLACE')
+
+    # Adding Modifiers
+
+    # Displacement Modifier
+
+    bpy.ops.object.modifier_add(type="DISPLACE")
     bpy.ops.texture.new()
-    bpy.data.textures["Texture"].type = 'CLOUDS'
+    bpy.data.textures["Texture"].type = "CLOUDS"
     bpy.data.textures["Texture"].noise_scale = 0.5
-    
-    #Adding Key frame to the displacemnt Modifier
-    
-    bpy.context.object.modifiers["Displace"].keyframe_insert(data_path="strength", frame=1)
-    
+
+    # Adding Key frame to the displacemnt Modifier
+
+    bpy.context.object.modifiers["Displace"].keyframe_insert(
+        data_path="strength", frame=1
+    )
+
     dpath = 'bpy.context.object.modifiers["Displace"].strength'
 
     fc = bpy.context.object.animation_data.action.fcurves[0]
-    
-    #Adding Noice Modifier to the Displacement fcurve
-    
+
+    # Adding Noice Modifier to the Displacement fcurve
+
     if fc:
         nmod = fc.modifiers.new("NOISE")
         nmod.scale = 10
@@ -125,65 +172,66 @@ def lightning_mesh(location=(0, 0, 0)):
         nmod.strength = 2
         nmod.depth = 1.35
         nmod.offset = 0.23
-        
-    #Adding Subsurf Modifier
-    
-    bpy.ops.object.modifier_add(type='SUBSURF')
-    
-    #Shading Smooth
-    
-    bpy.ops.object.shade_smooth()
-    
-    #Adding Buil Modifer
-    
-    bpy.ops.object.modifier_add(type='BUILD')
-    bpy.context.object.modifiers["Build"].frame_duration = 20
-    
-    #print("Lightning is summoned")
 
-    
-#Crating Material fr the lightning
+    # Adding Subsurf Modifier
+
+    bpy.ops.object.modifier_add(type="SUBSURF")
+
+    # Shading Smooth
+
+    bpy.ops.object.shade_smooth()
+
+    # Adding Buil Modifer
+
+    bpy.ops.object.modifier_add(type="BUILD")
+    bpy.context.object.modifiers["Build"].frame_duration = 20
+
+    # print("Lightning is summoned")
+
+
+# Crating Material fr the lightning
+
 
 def lightning_material():
-    
-    #getting the active object
-    
+
+    # getting the active object
+
     so = bpy.context.active_object
-    
-    #adding new material
-    
-    l_mat = bpy.data.materials.new(name = "Lightning Material")
+
+    # adding new material
+
+    l_mat = bpy.data.materials.new(name="Lightning Material")
     so.data.materials.append(l_mat)
     l_mat.use_nodes = True
-    nodes =l_mat.node_tree.nodes
+    nodes = l_mat.node_tree.nodes
     material_output = nodes.get("Material Output")
-    
-    #Creating emission Shader
-    
-    node_emission = nodes.new(type = 'ShaderNodeEmission')
-    
-    #Emission Shader colour
-    
-    node_emission.inputs[0].default_value = [0,0.3,1,1]
-    
-    #Emission Shader strength
-    
+
+    # Creating emission Shader
+
+    node_emission = nodes.new(type="ShaderNodeEmission")
+
+    # Emission Shader colour
+
+    node_emission.inputs[0].default_value = [0, 0.3, 1, 1]
+
+    # Emission Shader strength
+
     node_emission.inputs[1].default_value = 1
-    
-    #Linking Emission and Material Output Shader
-    
+
+    # Linking Emission and Material Output Shader
+
     links = l_mat.node_tree.links
-    new_link = links.new(node_emission.outputs[0],material_output.inputs[0])
+    new_link = links.new(node_emission.outputs[0], material_output.inputs[0])
     bpy.context.scene.eevee.use_bloom = True
-    
-    #Keyframing Emission Shader Strength
-    
+
+    # Keyframing Emission Shader Strength
+
     node_emission.inputs[1].keyframe_insert(data_path="default_value", frame=1)
     node_emission.select = True
     dpath = 'nodes["Emission"].inputs[1].default_value'
-    
-    #Adding Noice Modifier to the Emission Strength fcurve
-    
+
+    # Adding Noice Modifier to the Emission Strength fcurve
+
     fc = l_mat.node_tree.animation_data.action.fcurves.find(dpath)
     if fc:
         nmod = fc.modifiers.new("NOISE")
@@ -192,25 +240,25 @@ def lightning_material():
         nmod.strength = 500
         nmod.depth = 1.35
         nmod.offset = 0.23
-    
 
-    
+
 def lightning():
-    
+
     lightning_mesh()
     lightning_material()
-    
+
 
 class AddLightningObject(Operator, AddObjectHelper):
     """Create a new Mesh Object"""
+
     bl_idname = "mesh.add_object"
     bl_label = "Add Lghtning Object"
-    bl_options = {'REGISTER', 'UNDO'}
+    bl_options = {"REGISTER", "UNDO"}
 
     scale: FloatVectorProperty(
         name="scale",
         default=(1.0, 1.0, 1.0),
-        subtype='TRANSLATION',
+        subtype="TRANSLATION",
         description="scaling",
     )
 
@@ -218,16 +266,16 @@ class AddLightningObject(Operator, AddObjectHelper):
 
         lightning()
 
-        return {'FINISHED'}
+        return {"FINISHED"}
 
 
 # Registration
 
+
 def add_object_button(self, context):
     self.layout.operator(
-        AddLightningObject.bl_idname,
-        text="Add Lightning Object",
-        icon='MOD_NOISE')
+        AddLightningObject.bl_idname, text="Add Lightning Object", icon="MOD_NOISE"
+    )
 
 
 # This allows you to right click on a button and link to documentation
